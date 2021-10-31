60badbdf5f47249f7050826ebba9df8e72a0e963
prajwal <22263436+prajwal89@users.noreply.github.com>
2021-10-31 17:51:33 +0530

    new header and footer

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">
+            Copyright ©2021 All Rights Reserved
+        </p>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,11 +10,9 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Home</title>
 </head>
-
 <body>
-
-
-
+    <?php include '/includes/header.php'; ?>
+    <!-- page body -->
 
 
 
@@ -26,6 +23,8 @@
 
     
 
+    <!-- page body END -->
+    <?php include '/includes/footer.php'; ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
