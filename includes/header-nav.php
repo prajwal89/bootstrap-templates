f04e35d72dbc0c863236fd711ab7a138a8fa5dea
prajwal <22263436+prajwal89@users.noreply.github.com>
2021-10-31 18:03:42 +0530

    updated home page

---
+++ includes/header-nav.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">Git test repo</a>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,21 +11,25 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Home</title>
 </head>
+
 <body>
-    <?php include '/includes/header.php'; ?>
+    <?php include './includes/header-nav.php'; ?>
     <!-- page body -->
 
 
+    <div class="container my-5 py-5" style="max-width: 600px;">
+        <a href="tag-cloud.php" class="btn btn-block btn-outline-primary rounded-0 my-4 d-block">Tag Cloud</a>
+        <a href="particles.php" class="btn btn-block btn-outline-primary rounded-0 my-4 d-block">Particles</a>
+    </div>
 
 
 
 
 
 
-    
 
     <!-- page body END -->
-    <?php include '/includes/footer.php'; ?>
+    <?php include './includes/footer.php'; ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
